9836909a080ac0f9d5cc9be05eca2f1131f230ce
dein-ding <floyd.h@gmx.de>
2021-06-18 11:26:36 +0200

    removed Taxi-Hamm from preview

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,18 +1,18 @@
 $(document).ready(async function () {
-    /* just for testing */
-    /* document
+  /* just for testing */
+  /* document
         .querySelector(":root")
         .style.setProperty("--navBarBackground", "rgba(0, 0, 0, .3)"); */
-    //console.log(document.querySelector("body").dataset);
+  //console.log(document.querySelector("body").dataset);
 
-    setTimeout(() => {
-        if (!sessionStorage.hasBeenPrompted) {
-            custom.confirm(
-                "Attention",
-                "This website is work in progress, so be kind if there are bugs.",
-                "continue"
-            );
-            sessionStorage.hasBeenPrompted = true;
-        }
-    }, 500);
+  setTimeout(() => {
+    if (!sessionStorage.hasBeenPrompted) {
+      custom.confirm(
+        "Attention",
+        "This website is work in progress, so be kind if there are bugs.",
+        "continue"
+      );
+      sessionStorage.hasBeenPrompted = true;
+    }
+  }, 500);
 });
